345bc71a297b3556f633c24c5e9afd714ce22d3a
heyitsbuse <75186522+heyitsbuse@users.noreply.github.com>
2021-01-01 19:21:17 -0500

    Add New Sticky Scroll-to-Top Button
    Created div class "sticky-parent" for the button's parent (flex box grid) and id="sticky" for the button (flex-end child).
    
    Fixed to the bottom right of only the project screens and only appears after scrolling down a little bit.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
-console.log("Welcome!")
+// id="sticky" is the scroll to top button
+var sticky = document.getElementById("sticky")
+var rootElement = document.documentElement //For Chrome/Firefox/IE/Opera
+var bodyElement = document.body //For Safari
+
+// create new window scroll function named scroll Appear
+window.onscroll = function() { scrollAppear() };
+
+// define scroll Appear to be visible when user is 100px from top of document
+function scrollAppear() {
+  if (bodyElement.scrollTop > 100 || rootElement.scrollTop > 100) {
+      sticky.style.display = ("block");
+  } else {
+      sticky.style.display = ("none");
+  }
+}
+
+// define scroll to top function
+function scrollToTop() {
+  bodyElement.scrollTo( {
+    top: 0,
+    behavior: "smooth"
+  }) ||
+  rootElement.scrollTo( {
+    top: 0,
+    behavior: "smooth"
+  })
+}
+
+// add onclick function to scroll to top button
+sticky.addEventListener("click", scrollToTop)
