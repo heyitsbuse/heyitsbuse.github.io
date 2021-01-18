1e5e1282e2eb87c209c038ee97891db2f177b6ee
heyitsbuse <75186522+heyitsbuse@users.noreply.github.com>
2021-01-18 15:13:14 -0500

    Linting Js

--- js/script.js
+++ js/script.js
@@ -1,32 +1,34 @@
 //
 // id="sticky" is the scroll to top button
-var sticky = document.getElementById("sticky")
-var rootElement = document.documentElement //For Chrome/Firefox/IE/Opera
-var bodyElement = document.body //For Safari
+var sticky = document.getElementById('sticky')
+// For Chrome/Firefox/IE/Opera
+var rootElement = document.documentElement
+// For Safari
+var bodyElement = document.body
 
 // create new window scroll function named scroll Appear
-window.onscroll = function() { scrollAppear() };
+window.onscroll = function () { scrollAppear() }
 
 // define scroll Appear to be visible when user is 300px from top of document
-function scrollAppear() {
+function scrollAppear () {
   if (bodyElement.scrollTop > 300 || rootElement.scrollTop > 300) {
-      sticky.style.display = ("block");
+    sticky.style.display = ('block')
   } else {
-      sticky.style.display = ("none");
+    sticky.style.display = ('none')
   }
 }
 
 // define scroll to top function
-function scrollToTop() {
-  bodyElement.scrollTo( {
+function scrollToTop () {
+  bodyElement.scrollTo({
     top: 0,
-    behavior: "smooth"
+    behavior: 'smooth'
   }) ||
-  rootElement.scrollTo( {
+  rootElement.scrollTo({
     top: 0,
-    behavior: "smooth"
+    behavior: 'smooth'
   })
 }
 
 // add onclick function to scroll to top button
-sticky.addEventListener("click", scrollToTop)
+sticky.addEventListener('click', scrollToTop)
